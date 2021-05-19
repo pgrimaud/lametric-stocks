f3c96fdf5a339dc86d73c37b6212b2a769fc43ac
Julien Cousin-Alliot <Nisouc@outlook.com>
2021-05-19 18:10:50 +0200

    Add stock name frame

--- config/fields.php
+++ config/fields.php
@@ -7,4 +7,8 @@ return [
         'key'  => 'symbol',
         'type' => Field::TEXT_TYPE,
     ],
+    [
+        'key'  => 'stock_name',
+        'type' => Field::SWITCH_TYPE,
+    ],
 ];
--- src/LaMetric/Api.php
+++ src/LaMetric/Api.php
@@ -24,17 +24,22 @@ class Api
      */
     public function fetchData(array $parameters = []): FrameCollection
     {
-
         $url = 'https://finnhub.io/api/v1/quote?symbol=' . $parameters['symbol'] . '&token=' . $this->credentials['api_key'];
-        $res = $this->client->request('GET', $url);
 
+        $res = $this->client->request('GET', $url);
         $json = (string) $res->getBody();
 
         $data = json_decode($json);
 
-        return $this->mapData([
+        $responseData = [
             'price' => '$' . round($data->c, 2)
-        ]);
+        ];
+
+        if ($parameters['stock_name'] === 'true') {
+            array_unshift($responseData, $parameters['symbol']);
+        }
+
+        return $this->mapData($responseData);
     }
 
     /**
@@ -49,11 +54,14 @@ class Api
         /**
          * Transform data as FrameCollection and Frame
          */
-        $frame = new Frame();
-        $frame->setText($data['price']);
-        $frame->setIcon('34');
 
-        $frameCollection->addFrame($frame);
+        foreach ($data as $value) {
+            $frame = new Frame();
+            $frame->setText($value);
+            $frame->setIcon('34');
+
+            $frameCollection->addFrame($frame);
+        }
 
         return $frameCollection;
     }
